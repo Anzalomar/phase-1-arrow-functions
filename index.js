ff4f55542ccb81125a3e6552396c4b8c86eb78f6
anzalomar <oanisa143@gmail.com>
2024-06-22 16:26:52 +0300

    complet lab

---
+++ index.js
@@ -0,0 +1,14 @@
+// 1) has a function expression called divide
+const divide = function() {
+    // 2) divide divides 2000 by 100
+    return 2000 / 100;
+};
+
+// 3) has an arrow function called square
+const square = x => x * x; // 4) square arrow function takes one parameter and multiplies it times itself
+
+// 5) has an arrow function called add
+const add = (a, b) => a + b; // 6) add arrow function takes two parameters and adds them together
+
+// You can export the functions if needed
+// module.exports = { divide, square, add };
